2e2e25f5a997fbb08342af38473f78b2ab35c05a
dijdid <ceccotticharles@gmail.com>
2025-07-17 16:28:44 -0400

    Update streamlit_app.py
    readded submit button code

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,25 @@ ingredients_list = st.multiselect(
     my_dataframe
 )
 
+if ingredients_list:
+
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    st.write(ingredients_string)
+
+     
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
 #new section to display smoothie froot nutrition info
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
